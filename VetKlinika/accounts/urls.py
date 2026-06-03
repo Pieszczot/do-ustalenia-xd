bfdbf72371081de678cdf155284a24331d1ef546
Dawid Olejniczak <67868027+DO-Dawid@users.noreply.github.com>
2026-06-03 23:49:12 +0200

    Create: Created missing logout api
    Also requires X-CSRFToken to work

--- VetKlinika/accounts/urls.py
+++ VetKlinika/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import current_user, login_user, register_user
+from .views import current_user, login_user, logout_user, register_user
 
 
 urlpatterns = [
     path('register/', register_user, name='register-user'),
     path('login/', login_user, name='login-user'),
+    path('logout/', logout_user, name='logout-user'),
     path('me/', current_user, name='current-user'),
 ]
--- VetKlinika/accounts/views.py
+++ VetKlinika/accounts/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.contrib.auth import authenticate, get_user_model, login
+from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
@@ -95,6 +95,14 @@ def login_user(request):
 
     login(request, user)
     return JsonResponse({'user': _user_response(user)})
+
+
+@require_POST
+def logout_user(request):
+    if request.user.is_authenticated:
+        logout(request)
+
+    return JsonResponse({'authenticated': False})
 
 
 @require_GET
